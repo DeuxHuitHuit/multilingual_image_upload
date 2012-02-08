110d6398bc09d3d33c2c7f6b905ad38c8610ce75
Vlad Ghita <vlad_micutul@yahoo.com>
2012-02-08 10:36:29 +0200

    Updated RO translation

--- lang/lang.ro.php
+++ lang/lang.ro.php
@@ -7,42 +7,57 @@
 			'email' => 'vlad.ghita@xandergroup.ro',
 			'website' => ''
 		),
-		'release-date' => '2011-11-15'
+		'release-date' => '2012-02-08'
 	);
 
 	/**
-	 * Field: Image Upload
+	 * Field: Multilingual Image Upload
 	 */
 	$dictionary = array(
 
-		'Image Upload' =>
-		false,
-
-		'Minimum width (px)' =>
-		'Lungime minimă (px)',
-
-		'Minimum height (px)' =>
-		'Înălţime minimă (px)',
-		
-		'If empty or 0, no minimum limit will be set.' =>
-		'Dacă este necompletat sau 0, nu va fi setată limita minimă.',
-		
-		'If empty or 0, no maximum resize limit will be set.' =>
-		'Dacă este necompletat sau 0, nu va fi setată limita maximă de redimensionare.',
+		'%s Use value from reference language if selected language has empty value.' =>
+		'%s Foloseşte valoarea de la limba de referinţă dacă valoarea pentru limba selectată este goală.',
 
 		'%s Create unique filenames' =>
 		'%s Crează nume unice',
 
-		'Image must have a minimum width of %1$spx.' =>
-		'Imaginea trebuie să aibă o lungime de minim %1$spx.',
+		'If empty or 0, no maximum resize limit will be set.' =>
+		'Dacă este necompletat sau 0, nu va fi setată limita maximă de redimensionare.',
+
+		'If empty or 0, no minimum limit will be set.' =>
+		'Dacă este necompletat sau 0, nu va fi setată limita minimă.',
 
 		'Image must have a minimum height of %1$spx.' =>
 		'Imaginea trebuie să aibă o înălţime de minim %1$spx.',
-		
+
+		'Image must have a minimum width of %1$spx.' =>
+		'Imaginea trebuie să aibă o lungime de minim %1$spx.',
+
+		'Minimum height (px)' =>
+		'Înălţime minimă (px)',
+
+		'Minimum width (px)' =>
+		'Lungime minimă (px)',
+
 		'Uploaded file is not an image.' =>
 		'Fişierul încărcat nu este o imagine.',
-			
-		'%s Use value from reference language if selected language has empty value.' =>
-		'%s Foloseşte valoarea de la limba de referinţă dacă valoarea pentru limba selectată este goală.'
-		
+
+		'Consolidate entry data' =>
+		'Consolidează datele înregistrărilor',
+
+		'Check this field if you want to consolidate database by <b>keeping</b> entry values of removed/old Language Driver language codes. Entry values of current language codes will not be affected.' =>
+		'Bifaţi acest câmp dacă doriţi consolidarea bazei de date prin <b>păstrarea</b> valorilor de la limbile înşăturate. Valorile înregistrărilor limbilor curente nu vor fi afectate.',
+
+		'Multilingual Image Upload' =>
+		false,
+
+		'Unknown language' =>
+		'Limbă necunoscută',
+
+		'Modify just current language value' =>
+		'Modifică doar valoarea pentru limba curentă',
+
+		'Modify all values' =>
+		'Modifică toate valorile',
+
 	);
