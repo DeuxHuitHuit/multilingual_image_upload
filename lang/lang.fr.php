d876e0d35fdc7ed5b8facbe8c65f8f26e44c91f7
Nicolas Brassard <nico@nitriques.com>
2015-10-07 20:13:00 -0400

    Added French

---
+++ lang/lang.fr.php
@@ -0,0 +1,66 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Deux Huit Huit',
+			'email' => 'open-source (at) deuxhuithuit.com',
+			'website' => 'http://www.deuxhuithuit.com/'
+		),
+		'release-date' => '2015-10-07',
+	);
+
+	/**
+	 * Field: Multilingual Image Upload
+	 */
+	$dictionary = array(
+
+		'%s Use value from main language if selected language has empty value.' =>
+		'%s Utiliser la valeur de la langue par défaut si la langue sélectionnée est vide',
+
+		'%s Create unique filenames' =>
+		'%s Créer des noms de fichier unique',
+
+		'If empty or 0, no maximum resize limit will be set.' =>
+		'Si vide ou 0, aucune limite maximale ne sera mise en place',
+
+		'If empty or 0, no minimum limit will be set.' =>
+		'Si vide ou 0, aucune limite minimale ne sera mise en place',
+
+		'Image must have a minimum height of %1$spx.' =>
+		'L\'image doit avoir une hauteur minimale de %1$spx.',
+
+		'Image must have a minimum width of %1$spx.' =>
+		'L\'image doit avoir une largeur minimale de  %1$spx.',
+
+		'Minimum height (px)' =>
+		'Largeur minimale (px)',
+
+		'Minimum width (px)' =>
+		'Largeur maximale (px)',
+
+		'Uploaded file is not an image.' =>
+		'Le fichier n\'est pas une image.',
+
+		'Consolidate entry data' =>
+		'Consolidez les données',
+
+		'Check this field if you want to consolidate database by <b>keeping</b> entry values of removed/old Language Driver language codes. Entry values of current language codes will not be affected.' =>
+		false,
+
+		'Multilingual Image Upload' =>
+		false,
+
+		'Unknown language' =>
+		'Langue inconnue',
+
+		'Modify just current language value' =>
+		'Mofidier uniquement la langue courante',
+
+		'Modify all values' =>
+		'Modifier toutes les langues',
+
+		'Empty values defaults to %s' =>
+		'Les langues sans fichier utiliseront les valeur en %s',
+
+	);
